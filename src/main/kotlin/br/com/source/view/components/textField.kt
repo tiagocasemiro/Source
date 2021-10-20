d8e4adc33f95f9ee8c8c32144b5810b7afc1eae3
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-10-20 18:07:48 -0300

    Create comonent edittext custom

--- src/main/kotlin/br/com/source/view/components/textField.kt
+++ src/main/kotlin/br/com/source/view/components/textField.kt
@@ -1,9 +1,11 @@
 package br.com.source.view.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,89 +16,79 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
-@Composable
-fun SourceTextField(label: String) {
-    val text = remember { mutableStateOf("") }
 
-    OutlinedTextField(
-        value = text.value,
-        onValueChange = {
-            text.value = it
-        },
-        modifier = Modifier.padding(0.dp).fillMaxWidth(),
-        enabled = true,
-        readOnly = false,
-        textStyle = TextStyle(fontSize = 12.sp),
-        label = { Text(label, fontSize = 12.sp, modifier = Modifier.padding(0.dp).background(Color.Red)) },
-        placeholder = null,
-        leadingIcon = null,
-        trailingIcon = null,
-        isError = false,
-        visualTransformation = VisualTransformation.None,
-        keyboardOptions = KeyboardOptions.Default,
-        keyboardActions = KeyboardActions.Default,
-        singleLine = false,
-        maxLines = Int.MAX_VALUE,
-        interactionSource = remember { MutableInteractionSource() },
-        shape = MaterialTheme.shapes.small.copy(CornerSize(20)),
-        colors = TextFieldDefaults.outlinedTextFieldColors()
-    )
-}
+
 
 
 @Composable
-fun CustomTextField(
-    modifier: Modifier = Modifier,
+fun SourceTextField(
+    text: String,
+    label: String = "",
+    placeholder: String = "",
+    fontSize: TextUnit = 13.sp,
     leadingIcon: (@Composable () -> Unit)? = null,
     trailingIcon: (@Composable () -> Unit)? = null,
-    placeholderText: String = "Placeholder",
-    fontSize: TextUnit = MaterialTheme.typography.body2.fontSize
 ) {
-    val text = remember { mutableStateOf("") }
-
-    BasicTextField(
-        value = text.value,
-        modifier = modifier
-        .background(
-            MaterialTheme.colors.surface,
-            MaterialTheme.shapes.small,
-        ).fillMaxWidth(),
-        onValueChange = {
-            text.value = it
-        },
-        singleLine = true,
-        cursorBrush = SolidColor(MaterialTheme.colors.primary),
-        textStyle = LocalTextStyle.current.copy(
-            color = MaterialTheme.colors.onSurface,
-            fontSize = fontSize
-        ),
-        decorationBox = { innerTextField ->
-            Row(
-                modifier,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (leadingIcon != null) leadingIcon()
-                Box(Modifier.weight(1f)) {
-                    if (text.value.isEmpty()) Text(
-                        placeholderText,
-                        style = LocalTextStyle.current.copy(
-                            color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f),
-                            fontSize = fontSize
+    val textRemember = remember { mutableStateOf(text) }
+    val modifier = Modifier
+    Column {
+        Text(
+            label,
+            style = LocalTextStyle.current.copy(
+                color = MaterialTheme.colors.primary.copy(alpha = 0.8f),
+                fontSize = (fontSize.value - 3f).sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        BasicTextField(
+            value = textRemember.value,
+            modifier = modifier
+                .background(
+                    MaterialTheme.colors.surface,
+                    MaterialTheme.shapes.small,
+                )
+                .border(width = 1.dp, color = MaterialTheme.colors.primary, shape = RoundedCornerShape(4.dp))
+                .height(35.dp),
+            onValueChange = {
+                textRemember.value = it
+            },
+            singleLine = true,
+            cursorBrush = SolidColor(MaterialTheme.colors.primary),
+            textStyle = LocalTextStyle.current.copy(
+                color = MaterialTheme.colors.onSurface,
+                fontSize = fontSize
+            ),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier.padding(horizontal = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (leadingIcon != null) leadingIcon()
+                    Box(Modifier.weight(1f)) {
+                        if (textRemember.value.isEmpty()) Text(
+                            placeholder,
+                            style = LocalTextStyle.current.copy(
+                                color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f),
+                                fontSize = fontSize
+                            )
                         )
-                    )
-                    innerTextField()
+                        innerTextField()
+                    }
+                    if (trailingIcon != null) trailingIcon()
                 }
-                if (trailingIcon != null) trailingIcon()
             }
-        }
-    )
+        )
+    }
 }

22d71582acd9a75953bf02dcaac86724cb2ae5aa
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-10-21 18:24:20 -0300

    Fix object to help framework os desserialization

--- src/main/kotlin/br/com/source/model/domain/Domain.kt
+++ src/main/kotlin/br/com/source/model/domain/Domain.kt
@@ -3,13 +3,13 @@ package br.com.source.model.domain
 import java.io.File
 
 data class LocalRepository(
-    val name: String,
-    val workDir: String,
-    val credential: Credential
+    var name: String = "",
+    var workDir: String = "",
+    var credential: Credential = Credential()
     ) {
     fun workDir(): File {
         return File(workDir)
     }
 }
 data class RemoteRepository(val url: String, val localRepository: LocalRepository)
-data class Credential(val username: String, val password: String)
+data class Credential(var username: String = "", var password: String = "")
